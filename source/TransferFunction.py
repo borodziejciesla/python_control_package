ccb426f9f6b3985effc32493e2aaa15555f7add1
Maciej Różewicz <rozewicz.maciej@gmail.com>
2018-12-10 01:20:57 +0100

    Added overload of == operator to transfer function

--- source/TransferFunction.py
+++ source/TransferFunction.py
@@ -13,6 +13,30 @@ class TransferFunction(LTIObject.LTIObject):
         print('Denumerator:')
         print(self.denumerator)
 
+    # operator == overload
+    def __eq__(self, other):
+        self_num = self.numerator
+        other_num = other.getNumerator()
+
+        self_den = self.denumerator
+        other_den = other.getDenumerator()
+
+        if (len(self_num) == len(other_num)):
+            for i in range(0, len(other_num)):
+                if (self_num[i] != other_num[i]):
+                    return False
+        else:
+            return False
+
+        if (len(self_den) == len(other_den)):
+            for i in range(0, len(other_den)):
+                if (self_den[i] != other_den[i]):
+                    return False
+        else:
+            return False
+
+        return True
+
     def getNumerator(self):
         return self.numerator
 
--- tests/testTransferFunction.py
+++ tests/testTransferFunction.py
@@ -11,23 +11,7 @@ class TestLTISystem(unittest.TestCase):
 
         obj_ref = TransferFunction([2, 4], [1, 4, 4])
 
-        obj.printSystem()
-
-        ref_num = obj_ref.getNumerator()
-        obj_num = obj.getNumerator()
-
-        ref_den = obj_ref.getDenumerator()
-        obj_den = obj.getDenumerator()
-
-        # Compare
-        self.assertEqual(len(ref_num), len(obj_num))
-        for i in range(0, len(ref_num)):
-            self.assertEqual(ref_num[i], obj_num[i])
-
-        self.assertEqual(len(ref_den), len(obj_den))
-        for i in range(0, len(ref_den)):
-            self.assertEqual(ref_den[i], obj_den[i])
-
+        self.assertEqual(obj, obj_ref)
 
 if __name__ == '__main__':
     unittest.main()
