34f7a453d92310ead734ed1769964a0d5c58d036
Maciej Różewicz <rozewicz.maciej@gmail.com>
2019-01-09 22:59:45 +0100

    Added serial and parallel connection for State Space

--- source/StateSpace.py
+++ source/StateSpace.py
@@ -1,29 +1,71 @@
 import source.LTIObject as LTIObject
+import numpy as np
 
 class StateSpace(LTIObject.LTIObject):
     
     def __init__(self, A, B, C, D):
-        self.A = A
-        self.B = B
-        self.C = C
-        self.D = D
+        self._A = A
+        self._B = B
+        self._C = C
+        self._D = D
 
     # operator== overloaded
     def __eq__(self, other):
-        return ((self.A == other.A) and (self.B == other.B) and (self.C == other.C) and (self.D == other.D))
+        return (np.array_equal(self._A, other._A) and np.array_equal(self._B, other._B) and np.array_equal(self._C, other._C) and np.array_equal(self._D, other._D))
 
     # convert to string
     def __str__(self):
-        return 'State Matrix A:\n' + str(self.A) + '\nControl Matrix B:\n' + str(self.B) + '\nOutput Matrix C:\n' + str(self.C) + '\nDirect Control Matrix D:\n' + str(self.D)
+        return 'State Matrix A:\n' + str(self._A) + '\nControl Matrix B:\n' + str(self._B) + '\nOutput Matrix C:\n' + str(self._C) + '\nDirect Control Matrix D:\n' + str(self._D)
 
     def getA(self):
-        return self.A
+        return self._A
 
     def getB(self):
-        return self.B
+        return self._B
 
     def getC(self):
-        return self.C
+        return self._C
 
     def getD(self):
-        return self.D
+        return self._D
+
+    def serialConnection(self, consecutive_object):
+        A1 = self._A
+        B1 = self._B
+        C1 = self._C
+        D1 = self._D
+
+        A2 = consecutive_object._A
+        B2 = consecutive_object._B
+        C2 = consecutive_object._C
+        D2 = consecutive_object._D
+
+        # Verification of matrix dimensions is needed
+        z1 = np.zeros((A1.shape[0], A2.shape[1]))
+        new_A = np.concatenate( (np.concatenate([A1, z1], axis = 1), np.concatenate([B2 * C1, A2], axis = 1)) )
+        new_B = np.concatenate([B1, B2 * D1])
+        new_C = np.concatenate([D2 * C1, C2], axis = 1)
+        new_D = D2 * D1
+
+        return StateSpace(new_A, new_B, new_C, new_D)
+
+    def parallelConnection(self, consecutive_object):
+        A1 = self._A
+        B1 = self._B
+        C1 = self._C
+        D1 = self._D
+
+        A2 = consecutive_object._A
+        B2 = consecutive_object._B
+        C2 = consecutive_object._C
+        D2 = consecutive_object._D
+
+        # Verification of matrix dimensions is needed
+        z1 = np.zeros((A1.shape[0], A2.shape[1]))
+        z2 = np.zeros((A2.shape[0], A1.shape[1]))
+        new_A = np.concatenate( (np.concatenate([A1, z1], axis = 1), np.concatenate([z2, A2], axis = 1)) )
+        new_B = np.concatenate([B1, B2])
+        new_C = np.concatenate([C1, C2], axis = 1)
+        new_D = D1 + D2
+
+        return StateSpace(new_A, new_B, new_C, new_D)
---
+++ tests/testStateSpace.py
@@ -0,0 +1,113 @@
+import unittest
+from  source.StateSpace import StateSpace
+import numpy as np
+
+class TestLTISystem(unittest.TestCase):
+
+    def test_serial_connection_1(self):
+        A1 = np.array([[1, 2], [0, 1]])
+        B1 = np.array([[0], [1]])
+        C1 = np.transpose(np.array([[1], [0]]))
+        D1 = np.array([1])
+
+        A2 = np.array([[1, 2], [0, 1]])
+        B2 = np.array([[0], [1]])
+        C2 = np.transpose(np.array([[1], [0]]))
+        D2 = np.array([1])
+
+        A_ref = np.array([[1, 2, 0, 0], [0, 1, 0, 0], [0, 0, 1, 2], [1, 0, 0, 1]])
+        B_ref = np.array([[0], [1], [0], [1]])
+        C_ref = np.transpose(np.array([[1], [0], [1], [0]]))
+        D_ref = np.array([1])
+
+        obj_1 = StateSpace(A1, B1, C1, D1)
+        obj_2 = StateSpace(A2, B2, C2, D2)
+        obj_ref = StateSpace(A_ref, B_ref, C_ref, D_ref)
+
+        obj = obj_1.serialConnection(obj_2)
+
+        self.assertEqual(obj, obj_ref)
+
+    def test_multiply_operator(self):
+        A1 = np.array([[1, 2], [0, 1]])
+        B1 = np.array([[0], [1]])
+        C1 = np.transpose(np.array([[1], [0]]))
+        D1 = np.array([1])
+
+        A2 = np.array([[1, 2], [0, 1]])
+        B2 = np.array([[0], [1]])
+        C2 = np.transpose(np.array([[1], [0]]))
+        D2 = np.array([1])
+
+        A_ref = np.array([[1, 2, 0, 0], [0, 1, 0, 0], [0, 0, 1, 2], [1, 0, 0, 1]])
+        B_ref = np.array([[0], [1], [0], [1]])
+        C_ref = np.transpose(np.array([[1], [0], [1], [0]]))
+        D_ref = np.array([1])
+
+        obj_1 = StateSpace(A1, B1, C1, D1)
+        obj_2 = StateSpace(A2, B2, C2, D2)
+        obj_ref = StateSpace(A_ref, B_ref, C_ref, D_ref)
+
+        obj = obj_1 * obj_2       
+
+        self.assertEqual(obj, obj_ref)
+
+    def test_parallel_connection_1(self):
+        A1 = np.array([[1, 2], [0, 1]])
+        B1 = np.array([[0], [1]])
+        C1 = np.transpose(np.array([[1], [0]]))
+        D1 = np.array([1])
+
+        A2 = np.array([[1, 2], [0, 1]])
+        B2 = np.array([[0], [1]])
+        C2 = np.transpose(np.array([[1], [0]]))
+        D2 = np.array([1])
+
+        A_ref = np.array([[1, 2, 0, 0], [0, 1, 0, 0], [0, 0, 1, 2], [0, 0, 0, 1]])
+        B_ref = np.array([[0], [1], [0], [1]])
+        C_ref = np.transpose(np.array([[1], [0], [1], [0]]))
+        D_ref = np.array([2])
+
+        obj_1 = StateSpace(A1, B1, C1, D1)
+        obj_2 = StateSpace(A2, B2, C2, D2)
+        obj_ref = StateSpace(A_ref, B_ref, C_ref, D_ref)
+
+        obj = obj_1.parallelConnection(obj_2)
+
+        self.assertEqual(obj, obj_ref)
+
+    def test_add_operator(self):
+        A1 = np.array([[1, 2], [0, 1]])
+        B1 = np.array([[0], [1]])
+        C1 = np.transpose(np.array([[1], [0]]))
+        D1 = np.array([1])
+
+        A2 = np.array([[1, 2], [0, 1]])
+        B2 = np.array([[0], [1]])
+        C2 = np.transpose(np.array([[1], [0]]))
+        D2 = np.array([1])
+
+        A_ref = np.array([[1, 2, 0, 0], [0, 1, 0, 0], [0, 0, 1, 2], [0, 0, 0, 1]])
+        B_ref = np.array([[0], [1], [0], [1]])
+        C_ref = np.transpose(np.array([[1], [0], [1], [0]]))
+        D_ref = np.array([2])
+
+        obj_1 = StateSpace(A1, B1, C1, D1)
+        obj_2 = StateSpace(A2, B2, C2, D2)
+        obj_ref = StateSpace(A_ref, B_ref, C_ref, D_ref)
+
+        obj = obj_1 + obj_2
+
+        self.assertEqual(obj, obj_ref)
+
+    # def test_feedback(self):
+    #     obj_1 = TransferFunction([1], [1, 2])
+    #     obj_2 = TransferFunction([1], [1, 2])
+    #     obj_ref = TransferFunction([1, 2], [1, 4, 5])
+
+    #     obj = TransferFunction.feedback(obj_1, obj_2)
+
+    #     self.assertEqual(obj, obj_ref)
+
+if __name__ == '__main__':
+    unittest.main()
