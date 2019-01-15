4d700465781b072bf1f5b67c38a8107c22b3533c
Maciej Różewicz <rozewicz.maciej@gmail.com>
2019-01-15 20:40:40 +0100

    Added initial analysis file

---
+++ source/lti_analysis.py
@@ -0,0 +1,17 @@
+import source.LTIObject
+
+def step(object, end_time = 1):
+    pass
+
+def impulse(object, end_time = 1):
+    pass
+
+def eigenvaluess(object):
+    pass
+
+def bode(object, frequency_interval = []):
+    pass
+
+def nyquist(object, frequency_interval = []):
+    pass
+
